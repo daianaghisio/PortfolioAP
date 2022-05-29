29193a62fce7bfb8a0a196429774dea6702e5ca4
Daiana Ghisio <daianaghisio@gmail.com>
2022-05-29 15:10:09 -0300

    update

--- BackEnd/src/main/java/com/portfolio/dg/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfolio/dg/Controller/PersonaController.java
@@ -3,6 +3,7 @@ package com.portfolio.dg.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.portfolio.dg.Entity.Persona;
 import com.portfolio.dg.Interface.PersonaService;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class PersonaController {
 	@Autowired PersonaService personaService;
 	
@@ -47,4 +49,10 @@ public class PersonaController {
 		
 		return persona;
 	}
+	
+	@GetMapping("/personas/traer/perfil")
+	public Persona findPersona() {
+		return personaService.findPersona((long)1);
+	}
+	
 }
